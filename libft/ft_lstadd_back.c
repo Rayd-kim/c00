2e3de88f2fe9257b74f0c6e1842ab1d0ad65b0c3
Yousung Kim <youskim@c6r5s7.42seoul.kr>
2021-12-11 16:29:57 +0900

    modi_lstadd_back

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: youskim <youskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 12:43:25 by youskim           #+#    #+#             */
-/*   Updated: 2021/11/27 17:48:22 by youskim          ###   ########.fr       */
+/*   Updated: 2021/12/11 16:29:41 by youskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = new;
-	new->next = NULL;
 }
